bbedfa05c5f99d95793eed94bcaeeae6c711ab83
Claire Elliott <yue.katou@gmail.com>
2017-08-20 21:51:31 -0500

    Add files via upload

--- js/helper.js
+++ js/helper.js
@@ -1,9 +1,6 @@
 /*
-
 This file contains all of the code running in the background that makes resumeBuilder.js possible. We call these helper functions because they support your code in this course.
-
 Don't worry, you'll learn what's going on in this file throughout the course. You won't need to make any changes to it until you start experimenting with inserting a Google Map in Problem Set 3.
-
 Cameron Pittman
 */
 
@@ -13,25 +10,25 @@ These are HTML strings. As part of the course, you'll be using JavaScript functi
 replace the %data% placeholder text you see in them.
 */
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<span>%data%</span><hr>';
+var HTMLheaderRole = '<span id="title">%data%</span><hr>';
 
-var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
-var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
-var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
-var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
-var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
-var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
-var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
+var HTMLcontactGeneric = '<li class="flex-item"><span class="dark-text">%contact%</span><span class="white-text">%data%</span></li>';
+var HTMLmobile = '<li class="flex-item"><span class="dark-text">mobile</span><span class="white-text">%data%</span></li>';
+var HTMLemail = '<li class="flex-item"><span class="dark-text">email</span><span class="white-text">%data%</span></li>';
+var HTMLtwitter = '<li class="flex-item"><span class="dark-text">twitter</span><span class="white-text">%data%</span></li>';
+var HTMLgithub = '<li class="flex-item"><span class="dark-text">github</span><span class="white-text">%data%</span></li>';
+var HTMLblog = '<li class="flex-item"><span class="dark-text">blog</span><span class="white-text">%data%</span></li>';
+var HTMLlocation = '<li class="flex-item"><span class="dark-text">location</span><span class="white-text">%data%</span></li>';
 
 var HTMLbioPic = '<img src="%data%" class="biopic">';
 var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
 
-var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-column"></ul>';
+var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance</h3><ul id="skills" class="flex-column"></ul>';
 var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
 
 var HTMLworkStart = '<div class="work-entry"></div>';
 var HTMLworkEmployer = '<a href="#">%data%';
-var HTMLworkTitle = ' - %data%</a>';
+var HTMLworkTitle = ' &mdash; %data%</a>';
 var HTMLworkDates = '<div class="date-text">%data%</div>';
 var HTMLworkLocation = '<div class="location-text">%data%</div>';
 var HTMLworkDescription = '<p><br>%data%</p>';
@@ -44,7 +41,7 @@ var HTMLprojectImage = '<img src="%data%">';
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
 var HTMLschoolName = '<a href="#">%data%';
-var HTMLschoolDegree = ' -- %data%</a>';
+var HTMLschoolDegree = ' &mdash; %data%</a>';
 var HTMLschoolDates = '<div class="date-text">%data%</div>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
 var HTMLschoolMajor = '<em><br>Major: %data%</em>';
@@ -239,11 +236,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+  map.fitBounds(mapBounds);
+});
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,155 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+// init vars
+var formattedName = HTMLheaderName.replace("%data%", "Claire Elliott");
+var formattedRole = HTMLheaderRole.replace("%data%", "Aspiring Web Developer");
+var formattedImage = HTMLbioPic.replace("%data%", "images/claire.jpg");
+var formattedWelcome = HTMLwelcomeMsg.replace("%data%", "Gonna make sum shit up rn and totally smoke this later yo!!");
+var formattedEmail = HTMLemail.replace("%data%", "yue.katou@gmail.com");
+var formattedTwitter = HTMLtwitter.replace("%data%", "slanndalous");
+var formattedGithub = HTMLgithub.replace("%data%", "spitsfire");
+var formattedMobile = HTMLmobile.replace("%data%", "555-555-5555");
+var formattedLocation = HTMLlocation.replace("%data%", "Tulsa, Oklahoma");
+$("#header").prepend(formattedName, formattedRole, formattedImage, formattedWelcome);
+$("#topContacts").prepend(formattedMobile, formattedEmail, formattedTwitter, formattedGithub, formattedLocation);
+$("#footerContacts").prepend(formattedMobile, formattedEmail, formattedTwitter, formattedGithub, formattedLocation);
+$("#mapDiv").append(googleMap);
+
+// arrays
+var projects = {
+    projects: [{
+            title: "Portfolio Mockup",
+            date: "May 30, 2017",
+            description: "Based on a mockup PDF, I created my own one-page portfolio site using HTML and CSS. I did not use any frameworks. Everything is hand coded.",
+            images: ["images/projects1_1.jpg", "images/projects1_2.jpg"],
+            url: "https://github.com/spitsfire/udacity-fewd/tree/master/pj-portfolio"
+        },
+        {
+            title: "",
+            date: "",
+            description: "",
+            images: [],
+            url: ""
+        }
+    ]
+};
+var bio = {
+    name: "Mary Claire Elliott",
+    role: "Aspiring Web Developer",
+    contacts: {
+        email: "yue.katou@gmail.com",
+        twtter: "@slanndalous",
+        github: "spitsfire",
+        location: "Oklahoma"
+    },
+    bioPic: "images/claire.jpg",
+    welcomeMessage: "Gonna make sum shit up rn and totally smoke this later yo!!",
+    skills: [
+        "CELTA Certified", "Japanese as a Second Language", "Experience w/ Excel Formulas and Functions", "Familiar w/ HTML, CSS, C#, Ruby, and JavaScript"
+    ]
+};
+var work = {
+    jobs: [{
+            employer: "JET Programme",
+            url: "https://jetprogramusa.org/",
+            title: "Assistant Language Teacher",
+            location: "Kamiamakusa, Kumamoto, Japan",
+            dates: "Apr 2014 - Aug 2017",
+            description: "<strong>Planned</strong> and taught elementary-level English lessons and organized yearly curriculums for six different different elementary and junior high schools. <strong>Organized</strong> and facilitated English teaching workshops each year for over one hundred elementary school teachers in the Kamiamakusa city area. <strong>Taught</strong> frequent English classes that focused on current issues in American politics and the social climate. Some of these classes included racial discrimination after the Civil War and during the Second World War, the Ferguson protests, and the 2016 Presidential race."
+        },
+        {
+            employer: "Digital Land Management Co.",
+            url: "http://www.dlmplan.com/",
+            title: "Assistant to President",
+            location: "Edmond, Oklahoma",
+            dates: "Mary 2011 - Apr 2014",
+            description: "<strong>Promoted</strong> to personal assistant of the President because of my ability to quickly absorb, understand and apply new theories. Oversaw and executed the majority of his responsibilities with his guidance while he recovered from a serious accident. <strong>Primary</strong> responsibilies included importing large amounts of sensitive data, overseeing client leasing projects, and training clients to use land management software. <strong>Independently</strong> oversaw projects across the country, temporarily relocated to Pittsburgh, PA to provide onsite assistance for clients."
+        }
+    ]
+};
+var education = {
+    schools: [{
+            name: "University of Cambridge's CELTA Program",
+            location: "Krakow, Poland",
+            degree: "Certified English Language Teacher for Adults",
+            dates: "Jan 2012",
+            majors: "N/A",
+            url: "https://www.britishcouncil.pl/en/celta"
+        },
+        {
+            name: "University of Oklahoma",
+            location: "Norman, Oklahoma",
+            degree: "Bachelors",
+            dates: "Aug 2007 - Dec 2012",
+            majors: ["East Asian Studies"],
+            url: "https://www.ou.edu/cis/ias/undergrad-academics/majors/asian_studies.html"
+        },
+        {
+            name: "Yamaguchi University",
+            location: "Yamaguchi City, Yamaguchi, Japan",
+            degree: "Study Abroad",
+            dates: "Sept 2010 - Mar 2011",
+            majors: ["East Asian Studies"],
+            url: "http://www.yamaguchi-u.ac.jp/english.html"
+        }
+    ],
+    onlineCourses: [{
+        title: "",
+        school: "",
+        dates: "",
+        url: ""
+    }]
+};
+if (bio.skills) {
+    $("#header").append(HTMLskillsStart);
+    var displaySkills = bio.skills.forEach(function(skill) {
+        var formattedSkills = HTMLskills.replace("%data%", skill);
+        $("#skills").append(formattedSkills);
+    });
+}
+
+var displayEducation = education.schools.forEach(function(edu) {
+    $("#education").append(HTMLschoolStart);
+    var formattedName = HTMLschoolName.replace("%data%", edu.name);
+    var formattedURL = formattedName.replace("#", edu.url);
+    var formattedDegree = HTMLschoolDegree.replace("%data%", edu.degree);
+    var formattedDates = HTMLschoolDates.replace("%data%", edu.dates);
+    var formattedLoc = HTMLschoolLocation.replace("%data%", edu.location);
+    var formattedMajor = HTMLschoolMajor.replace("%data%", edu.majors);
+    var formattedNameDegree = formattedURL + formattedDegree;
+    $(".education-entry:last").append(formattedNameDegree,formattedDates, formattedLoc,formattedMajor);
+});
+
+var displayWork = work.jobs.forEach(function(career) {
+    $("#workExperience").append(HTMLworkStart);
+    var formattedEmployer = HTMLworkEmployer.replace("%data%", career.employer);
+    var formattedURL = formattedEmployer.replace("#", career.url);
+    var formattedTitle = HTMLworkTitle.replace("%data%", career.title);
+    var formattedDates = HTMLworkDates.replace("%data%", career.dates);
+    var formattedLocation = HTMLworkLocation.replace("%data%", career.location);
+    var formattedDescription = HTMLworkDescription.replace("%data%", career.description);
+    var formattedEmployerTitle = formattedURL + formattedTitle;
+    $(".work-entry:last").append(formattedEmployerTitle,formattedDates,formattedLocation,formattedDescription);
+});
+$(document).click(function(loc) {
+    var x = loc.pageX;
+    var y = loc.pageY;
+    logClicks(x, y);
+});
+
+var displayProjects = projects.projects.forEach(function(proj) {
+    $("#projects").append(HTMLprojectStart);
+    var formattedTitle = HTMLprojectTitle.replace("%data%", proj.title);
+    var formattedURL = formattedTitle.replace("#", proj.url);
+    var formattedDate = HTMLprojectDates.replace("%data%", proj.date);
+    var formattedDescription = HTMLprojectDescription.replace("%data%", proj.description);
+    $(".project-entry:last").append(formattedURL, formattedDate, formattedDescription);
+    if (proj.images.length > 0) {
+        proj.images.forEach(function(img) {
+            var formattedProjImage = HTMLprojectImage.replace("%data%", img);
+            $(".project-entry:last").append(formattedProjImage);
+        });
+    }
+});
+
+displayEducation();
+displayWork();
+displayProjects();
